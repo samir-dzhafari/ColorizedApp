1523a7932c20d6596a999cbd4523c1ad7245dc95
Samir Dzhafari <samirjafari2001@icloud.com>
2023-10-18 11:09:36 +0300

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Самир Джафари on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
